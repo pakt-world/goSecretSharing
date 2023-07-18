9932b35d2c2e704149fb3e68e68fdc0e77569132
Joshua Akinsuyi <akinsuyi.joshua84@gmail.com>
2023-07-18 12:00:15 +0300

    fix: Added Tests

--- index_test.go
+++ index_test.go
@@ -1,12 +1,23 @@
 package goSecretSharing
 
-func TestSharingAndRecover() {
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSharingAndRecover(t *testing.T) {
 	secretText := "Hello World"
 	noOfShares := 5
 	noRequiredShares := 2
-	splitedStrings := SplitSecret(secretText, noOfShares, noRequiredShares)
+	splitedStrings, tErr := SplitSecret(secretText, noOfShares, noRequiredShares)
+	if tErr != nil {
+		t.Errorf("splitting Secret Failed... %v", tErr)
+	}
 	// recover splitedStrings
 	recString := []string{splitedStrings[1], splitedStrings[2]}
-	recoveredSecret := RecoverSecret(recString)
-	return
+	recoveredSecret, eErr := RecoverSecret(recString)
+	if eErr != nil {
+		t.Errorf("splitting Secret Failed... %v", tErr)
+	}
+	fmt.Println("test successfull..", recoveredSecret)
 }
